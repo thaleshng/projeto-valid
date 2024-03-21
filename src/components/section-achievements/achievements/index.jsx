47cc586aa71fbe363a2df25de0cf4b106eebb580
Thales Gomes <thaleshng@gmail.com>
2024-03-20 21:04:51 -0300

    adicionando efeito na section achievements

--- src/components/section-achievements/achievements/index.jsx
+++ src/components/section-achievements/achievements/index.jsx
@@ -1,6 +1,29 @@
 import styled from "styled-components"
+import { useState, useEffect } from "react";
 
 export const Achievements = () => {
+    const [opacity, setOpacity] = useState(1);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const elementOffset = document.getElementById("achievements").offsetTop;
+
+            const distanceFromTop = elementOffset - scrollPosition;
+            const maxOpacityDistance = windowHeight * 0.5; // Adjust this value as needed for when you want the opacity to start changing
+
+            const newOpacity = 1 - (distanceFromTop / maxOpacityDistance);
+            setOpacity(newOpacity >= 0 ? newOpacity : 0);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <>
             <AchievementTitle>
@@ -8,20 +31,15 @@ export const Achievements = () => {
                 <p>Tornamos o cotidiano de milhões de pessoas mais seguro</p>
             </AchievementTitle>
 
-            <DivAchievements>
-                <h3>#1 em emissão de identificação CNHs e RGs no Brasil</h3>
-
-                <h3>Top 5 maiores fabricantes de SIM cards do mundo</h3>
-
-                <h3>900M de aparelhos conectados com a tecnologia Valid</h3>
-
-                <h3>Top 10 global em produção de cartões de crédito e débito</h3>
+            <DivAchievements id="achievements">
+                <h3 style={{ opacity }}>#1 em emissão de identificação CNHs e RGs no Brasil</h3>
+                <h3 style={{ opacity }}>Top 5 maiores fabricantes de SIM cards do mundo</h3>
+                <h3 style={{ opacity }}>900M de aparelhos conectados com a tecnologia Valid</h3>
+                <h3 style={{ opacity }}>Top 10 global em produção de cartões de crédito e débito</h3>
             </DivAchievements>
-            
         </>
-        
-    )
-}
+    );
+};
 
 const AchievementTitle = styled.div`
     margin-bottom: 175px;
